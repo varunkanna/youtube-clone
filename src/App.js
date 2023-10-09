e341c7f685eba45f3f93aece13601da5ad2279ca
Gangeswaran K <96485946+varunkanna@users.noreply.github.com>
2023-10-09 07:42:14 +0530

    Project folder setup added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
+import "./App.css";
+import Header from "./components/headers/Header";
+import Sidebar from "./components/sidebar/sidebar";
+import HomeScreens from "./screens/homeScreens/HomeScreens";
+import './Variable.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="app_container border border-info ">
+        <Sidebar />
+        <Container className="app__main">
+          <HomeScreens />
+        </Container>
+      </div>
+    </>
   );
 }
 
---
+++ src/screens/homeScreens/HomeScreens.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import CategoriesBar from "../../components/categoriesBar/CategoriesBar";
+import Video from "../../components/videos/Video";
+
+const HomeScreens = () => {
+  return (
+    <Container fluid>
+      <CategoriesBar />
+      <Row>
+        {[...new Array(20)].map(() => (
+          <Col lg={3} md={4}>
+            <Video />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default HomeScreens;
